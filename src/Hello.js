acd5c5066d739e5ccca839d19f55e9f2d37a6ef6
Yi Sun <itssybruce@gmail.com>
2020-02-16 23:00:09 -0600

    first commit

---
+++ src/Hello.js
@@ -0,0 +1,16 @@
+import React, { Component }from 'react';
+import './Hello.css';
+
+class Hello extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Hello World </h1>
+				<p>Welcome to this world</p>
+			</div>
+		);
+
+	}
+}
+
+export default Hello;
