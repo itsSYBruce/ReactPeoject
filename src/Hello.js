c2e559d0ad68dcad0c5dca521dbad4a71c159dec
Yi Sun <itssybruce@gmail.com>
2020-02-16 23:24:00 -0600

    props

--- src/Hello.js
+++ src/Hello.js
@@ -4,9 +4,10 @@ import './Hello.css';
 class Hello extends Component {
 	render() {
 		return (
-			<div>
+			<div className='f1 tc'>
 				<h1>Hello World </h1>
 				<p>Welcome to this world</p>
+				<p>{this.props.greeting}</p>
 			</div>
 		);
 
--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import './index.css';
 import Hello from './Hello';
 import * as serviceWorker from './serviceWorker';
 
+import 'tachyons';
 
 
-ReactDOM.render(<Hello />, document.getElementById('root'));
+
+ReactDOM.render(<Hello greeting = {"Hello Nairoman"}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
